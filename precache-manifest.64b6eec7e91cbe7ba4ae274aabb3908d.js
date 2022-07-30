647772fa1529d9aaa54ce67d8f1bb00a3b85984b
Jiban Kumar Debashis Behera <66745177+jibanblog@users.noreply.github.com>
2022-07-31 01:56:31 +0530

    Updates

---
+++ precache-manifest.64b6eec7e91cbe7ba4ae274aabb3908d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0e34c54bd3fc7bcbc610af5b351b8e",
+    "url": "/reduxcontactapp/index.html"
+  },
+  {
+    "revision": "95873430f16a52063e82",
+    "url": "/reduxcontactapp/static/css/main.f03df457.chunk.css"
+  },
+  {
+    "revision": "01aeb17621f5c8574aca",
+    "url": "/reduxcontactapp/static/js/2.d3af8d8d.chunk.js"
+  },
+  {
+    "revision": "ab8f792973d32f9b93c607ee81282492",
+    "url": "/reduxcontactapp/static/js/2.d3af8d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95873430f16a52063e82",
+    "url": "/reduxcontactapp/static/js/main.8728d418.chunk.js"
+  },
+  {
+    "revision": "8e8098724ec95ee8f7c0",
+    "url": "/reduxcontactapp/static/js/runtime-main.04b97931.js"
+  }
+]);
